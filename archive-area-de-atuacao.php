4abf7aee319a0a40db37667e40ec1f3591e568cb
Bruno Oliveira <contato@brunooliveira.dev.br>
2024-08-16 23:05:01 -0300

    :sparkles: Expertise component for home

--- archive-area-de-atuacao.php
+++ archive-area-de-atuacao.php
@@ -16,7 +16,7 @@
                             <?php while (have_posts()): the_post();?>
                                 <div data-expertise="<?php echo get_the_ID(); ?>" class="col-4 col-md-2 mt-2 mb-2">
                                     <a href="<?php echo get_the_permalink(); ?>">
-                                    <div class="p-3 rounded-2 d-flex align-items-center justify-content-start flex-row expertise-list-item">
+                                    <div class="p-3 rounded-2 d-flex align-items-center justify-content-start flex-column expertise-list-item">
                                         <img class="expertise-list-item-icon" src="<?php echo get_field('icon_main', get_the_ID()); ?>" />
                                         <img class="expertise-list-item-icon-hover" src="<?php echo get_field('icon_hover', get_the_ID()); ?>" />
                                         <span><?php echo get_the_title(); ?></span>
