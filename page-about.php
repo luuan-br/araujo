1beb5d211d73f7c5a37ad298edac75eac22921a0
Giovani Lima <giovani_asl@hotmail.com>
2024-08-19 20:59:19 -0300

    :recycle: Adjusts layout About Page

--- page-about.php
+++ page-about.php
@@ -49,37 +49,42 @@
 
     $query = new WP_Query( $args );                            
 ?>
+    
+<div class="container divider-block">
+</div>
 
 <div id="posts">
+
     <div class="container" style="overflow: hidden;">
-        <div class="row">
-            <div class="col-12">
-                <h2 class="sub-title">Reconhecimentos</h2>
-            </div>
-        </div>
         
         <div class="row">
             <div class="col-12">
                 <?php if ( $query->have_posts() ): ?>
-                    <div id="post-carousel" class="posts f-carousel">
-                        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                            <div class="f-carousel__slide">
-                                <a href="<?= get_permalink(); ?>" class="post">
-                                    <?php if( has_post_thumbnail() ): ?>
-                                        <img src="<?= get_the_post_thumbnail_url(); ?>" alt="<?= get_the_title(); ?>" class="post__image">
-                                    <?php else: ?>
-                                        <img src="<?= get_template_directory_uri() . '/assets/images/default.webp'; ?>" alt="<?= get_the_title(); ?>" class="post__image">
-                                    <?php endif; ?>
 
-                                    <div class="post__content">
-                                        <div class="execpt">
-                                            <?= get_the_excerpt(); ?>
+                    <div id="block-title-slider">
+
+                        <h2 class="sub-title mb-4">Reconhecimentos</h2>
+
+                        <div id="post-carousel" class="posts f-carousel">
+                            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                                <div class="f-carousel__slide">
+                                    <a href="<?= get_permalink(); ?>" class="post">
+                                        <?php if( has_post_thumbnail() ): ?>
+                                            <img src="<?= get_the_post_thumbnail_url(); ?>" alt="<?= get_the_title(); ?>" class="post__image">
+                                        <?php else: ?>
+                                            <img src="<?= get_template_directory_uri() . '/assets/images/default.webp'; ?>" alt="<?= get_the_title(); ?>" class="post__image">
+                                        <?php endif; ?>
+
+                                        <div class="post__content">
+                                            <div class="execpt">
+                                                <?= get_the_excerpt(); ?>
+                                            </div>
+                                            <div class="date"><?= get_the_date( 'd/m/Y' ); ?></div>
                                         </div>
-                                        <div class="date"><?= get_the_date( 'd/m/Y' ); ?></div>
-                                    </div>
-                                </a>
-                            </div>
-                        <?php endwhile; ?>
+                                    </a>
+                                </div>
+                            <?php endwhile; ?>
+                        </div>
                     </div>
 
                     <?php 
@@ -88,13 +93,13 @@
                     ?>
 
                     <a href="<?= esc_url($category_link); ?>" class="btn btn-load-more-posts">
-                        <span>Carregar mais reconhecimentos</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11" fill="none">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.32828 10.5741C9.70942 10.9197 10.2906 10.9197 10.6717 10.5741L19.183 2.85655C19.6207 2.45966 19.6207 1.77183 19.183 1.37494L18.3384 0.609083C17.9573 0.263488 17.3761 0.263488 16.9949 0.609083L10.6717 6.34265C10.2906 6.68825 9.70942 6.68825 9.32828 6.34265L3.00506 0.609083C2.62392 0.263487 2.04275 0.263486 1.66161 0.609083L0.816989 1.37494C0.379285 1.77183 0.379285 2.45966 0.816989 2.85655L9.32828 10.5741Z" fill="#F45B06"/>
+                        <span>Ver mais reconhecimentos</span>
+                        <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.9827 10.263C11.3283 9.88189 11.3283 9.30072 10.9827 8.91959L3.26512 0.408297C2.86823 -0.029407 2.1804 -0.0294064 1.78351 0.408298L1.01765 1.25292C0.672057 1.63406 0.672057 2.21523 1.01765 2.59636L6.75122 8.91959C7.09682 9.30072 7.09682 9.88189 6.75122 10.263L1.01765 16.5863C0.672058 16.9674 0.672057 17.5486 1.01765 17.9297L1.78351 18.7743C2.1804 19.212 2.86823 19.212 3.26512 18.7743L10.9827 10.263Z" fill="#F45B06"/>
                         </svg>
                     </a>
                 <?php else : ?>                      
-                    <p>Nenhum post encontrado.</p>
+                    <p>Nenhum reconhecimento encontrado.</p>
                 <?php endif;?>
             </div>
         </div>
